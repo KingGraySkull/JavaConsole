2ab0443c076ef6c83d43977d4608db8181e83105
webwerks <webwerks@webwerks-DL-IPMSB-H61>
2018-08-27 16:38:23 +0530

    Base version

--- ConsoleApp/src/main/ConsoleApp.java
+++ ConsoleApp/src/main/ConsoleApp.java
@@ -1,47 +1,9 @@
 package main;
 
-import multithreading.Processor;
-
 public class ConsoleApp
 {
 	public static void main(String[] args)
 	{
-		final Processor processor = new Processor();
-		Thread t1 = new Thread(new Runnable() {
-
-			@Override
-			public void run()
-			{
-				try
-				{
-					processor.publish();
-				} catch (InterruptedException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-		});
-		t1.start();
 		
-		Thread t2 = new Thread(new Runnable() {
-
-			@Override
-			public void run()
-			{
-				try
-				{
-					processor.subscribe();
-				} catch (InterruptedException e)
-				{
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			}
-			
-		});
-		t2.start();
-
 	}
 }
